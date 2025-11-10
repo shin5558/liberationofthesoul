41f25e14adadd251d6e6646b9df6aab7cf73e2f9
shin5558 <shin.5558@gmail.com>
2025-11-10 14:38:04 +0900

    "トップの「ゲームスタート」から characters#new へ遷移できるようにルート/コントローラ/ビューを追加（M2-04）" git push origin main

---
+++ app/controllers/characters_controller.rb
@@ -0,0 +1,10 @@
+class CharactersController < ApplicationController
+  def new
+    # まずは画面が出るだけでOK（AC要件は遷移）
+    # 後でフォームや初期値を入れていきます
+  end
+
+  # 今は使わないが雛形として置いておく（コメントアウトでもOK）
+  # def create
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 # config/routes.rb
 Rails.application.routes.draw do
   root 'home#index'
+
+  # キャラクター作成への導線（new, create は今後使うので先に定義）
+  resources :characters, only: %i[new create]
 end
